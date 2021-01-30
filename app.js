793186eb925d73fc2886a409ac63613ee12cdaf7
Ryan Rotman <ryan.rotman@gmail.com>
2021-01-30 15:02:15 -0600

    todo holder

--- app.js
+++ app.js
@@ -2,3 +2,6 @@
 let todoInput = document.querySelector("#todo-text");
 let todoForm = document.querySelector("#todo-form");
 let todoList = document.querySelector("#todo-list");
+
+// Empty array to hold todos
+let todos = [];
