70d3bbac5825f12ef4a00c749ccd008158d873dc
Ryan Rotman <ryan.rotman@gmail.com>
2021-01-30 15:01:27 -0600

    needed items from html

---
+++ app.js
@@ -0,0 +1,4 @@
+// Selected items from HTML
+let todoInput = document.querySelector("#todo-text");
+let todoForm = document.querySelector("#todo-form");
+let todoList = document.querySelector("#todo-list");
