a8036b3120639bcb594a59beae152103967788db
Ryan Rotman <ryan.rotman@gmail.com>
2021-01-31 12:02:12 -0600

    app init by loading stored todos if there are any

--- app.js
+++ app.js
@@ -5,3 +5,44 @@ let todoList = document.querySelector("#todo-list");
 
 // Empty array to hold todos
 let todos = [];
+
+// App init
+init();
+
+// // Get stored todos from localstorage
+function init() {
+    let storedTodos = JSON.parse(localStorage.getItem("todos"));
+
+    if (storedTodos !== null) {
+        todos = storedTodos;
+    }
+
+    renderTodos();
+}
+
+renderTodos();
+
+function renderTodos() {
+    todoList.innerHTML = "";
+    
+    // Render a new tr for each to-do
+    for (let i = 0; i < todos.length; i++) {
+        let todo = todos[i];
+
+        let tr = document.createElement("tr");
+        tr.innerHTML = `
+            <td>${todo}</td>
+            <td><button type="button" class="btn btn-warning">Complete</button>&nbsp<button type="button" class="btn btn-danger">Delete</button></td>
+        `
+
+        // let completeBtn = document.createElement("button");
+        // completeBtn.textContent = "Complete";
+
+        // let deleteBtn = document.createElement("button");
+        // deleteBtn.textContent = "Delete";
+
+        // tr.appendChild(completeBtn);
+        // tr.appendChild(deleteBtn);
+        todoList.appendChild(tr);
+    }
+}
